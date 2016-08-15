3bf2c187855c610a45f155f0147b14c8700c9b73
Flux <flux.blackcat@gmail.com>
2016-08-15 15:18:47 -0700

    modularized vr viewer

--- modules/index.js
+++ modules/index.js
@@ -1,5 +1,12 @@
 import VRViewer from './vrviewer';
+const createApp = VRViewer( THREE );
 
-const { scene, camera, controller1, controller2 } = VRViewer.start();
+const { scene, camera, controller1, controller2, events } = createApp({
+  autoEnter: false,
+  emptyRoom: true
+});
 
 scene.add( new THREE.Mesh( new THREE.BoxGeometry( 1, 1, 1 ), new THREE.MeshStandardMaterial() ) );
+
+events.on( 'tick', (dt)=>{
+});
