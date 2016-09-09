3bac9f37d47e8e5c0b26c4b542ffd26daa6bb28b
Flux <flux.blackcat@gmail.com>
2016-09-08 18:42:23 -0700

    created a down arrow for dropdown and fixed its depth

--- modules/datguivr/dropdown.js
+++ modules/datguivr/dropdown.js
@@ -86,11 +86,28 @@ export default function createCheckbox( {
   //  base option
   const selectedLabel = createOption( initialLabel, false );
   selectedLabel.position.x = Layout.PANEL_MARGIN + width * 0.5;
-  selectedLabel.position.z = depth*2;
+  selectedLabel.position.z = depth;
+
+  selectedLabel.add((function createDownArrow(){
+    const w = 0.015;
+    const h = 0.03;
+    const sh = new THREE.Shape();
+    sh.moveTo(0,0);
+    sh.lineTo(-w,h);
+    sh.lineTo(w,h);
+    sh.lineTo(0,0);
+
+    const geo = new THREE.ShapeGeometry( sh );
+    Colors.colorizeGeometry( geo, Colors.DROPDOWN_FG_COLOR );
+    geo.translate( DROPDOWN_WIDTH - w * 4, -DROPDOWN_HEIGHT * 0.5 + h * 0.5 , depth * 1.01 );
+
+    return new THREE.Mesh( geo, SharedMaterials.PANEL );
+  })());
 
   selectedLabel.add( ...Object.keys(options).map( function( optionName, index ){
     const optionLabel = createOption( optionName, true );
     optionLabel.position.y = -DROPDOWN_MARGIN - (index+1) * ( DROPDOWN_OPTION_HEIGHT );
+    optionLabel.position.z = depth;
     return optionLabel;
   }));
 
