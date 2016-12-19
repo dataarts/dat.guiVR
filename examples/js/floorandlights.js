94015572df06b8aa638d4dfb78f88e86ecc3dea2
Jeff Nusz <jeff@custom-logic.com>
2016-12-19 12:26:32 -0800

    tidy up examples

--- examples/js/floorandlights.js
+++ examples/js/floorandlights.js
@@ -20,13 +20,16 @@
 function createFloorAndLights(){
   const group = new THREE.Group();
 
-  const floor = new THREE.Mesh( new THREE.PlaneGeometry( 8,8, 1, 1), new THREE.MeshStandardMaterial({side: THREE.DoubleSide}) );
+  const floor = new THREE.Mesh( new THREE.PlaneGeometry( 8,8, 1, 1), new THREE.MeshStandardMaterial(
+    {side: THREE.DoubleSide, color:0x757575}) );
+  floor.material.metalness = 0.15;
+  floor.material.roughness = 0.8;
   floor.rotation.x = -Math.PI * 0.5;
   floor.receiveShadow = true;
   group.add( floor );
 
   const light = new THREE.SpotLight( 0xffffff );
-  light.intensity = 2.0;
+  light.intensity = 1.0;
   light.distance = 7;
   light.position.set( 0, 6, -1 );
   light.penumbra = 0.248;
