b85438961d42afb8cba23d1a68ec47f52ee86955
Yulius Tjahjadi <yulius@fxpal.com>
2017-11-17 14:59:57 -0800

    fix dropdown name label change

--- modules/datguivr/dropdown.js
+++ modules/datguivr/dropdown.js
@@ -257,7 +257,7 @@ export default function createCheckbox( {
   };
 
   group.name = function( str ){
-    descriptorLabel.update( str );
+    descriptorLabel.updateLabel( str );
     return group;
   };
 
